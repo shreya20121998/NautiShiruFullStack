d3da9d85e502ef25d486429a05e28bfb1f89e90c
Deepu Goyal <goyaldeepanshu2906@gmail.com>
2023-12-24 09:14:02 +0000

    createVoyage page master table done

--- app/nauticalfe/webapp/controller/CreateVoyage.controller.js
+++ app/nauticalfe/webapp/controller/CreateVoyage.controller.js
@@ -1,44 +1,270 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+  "sap/ui/core/mvc/Controller"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
- 
-        return Controller.extend("nauticalfe.controller.CreateVoyage", {
-            onInit: function () {
-                var oView = this.getView();
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-      // Button event handlers
-      // oView.byId("_IDGenButton1").attachPress(function () {
-      //   alert("Create Voyage button clicked!");
-      // });
-      
-      
+    return Controller.extend("nauticalfe.controller.CreateVoyage", {
+      onInit: function () {
+        var oView = this.getView();
 
-      // oView.byId("_IDGenButton3").attachPress(function () {
-      //   alert("Calculate button clicked!");
-      // });
+        // Button event handlers
+        // oView.byId("_IDGenButton1").attachPress(function () {
+        //   alert("Create Voyage button clicked!");
+        // });
 
-      // oView.byId("_IDGenButton4").attachPress(function () {
-      //   alert("Refresh button clicked!");
-      // });
-    },
-    onFreightSimulator: function() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter.navTo("RouteFreightSimulator");
-    },
-    onChangeVoyage: function() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter.navTo("RouteTrChangeVoyage");
-    },
 
-    
+
+        // oView.byId("_IDGenButton3").attachPress(function () {
+        //   alert("Calculate button clicked!");
+        // });
+
+        // oView.byId("_IDGenButton4").attachPress(function () {
+        //   alert("Refresh button clicked!");
+        // });
+      },
+      onFreightSimulator: function () {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("RouteFreightSimulator");
+      },
+      onChangeVoyage: function () {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("RouteTrChangeVoyage");
+      },
+      populateInputField: function (inputField, selectedValue) {
+        inputField.setValue(selectedValue);
+      },
+      showValueHelpDialog1: function () {
+        // Create a dialog
+        console.log("clicked voyage");
+        var oDialog = new sap.m.Dialog({
+          title: "Select: Voyage Types",
+          contentWidth: "60%",
+          contentHeight: "60%",
+          content: new sap.m.Table({
+            mode: sap.m.ListMode.SingleSelectMaster,
+
+            columns: [
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Voyage Type" }),
+              }),
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Description" }),
+              }),
+            ],
+
+            selectionChange: function (oEvent) {
+              var oSelectedItem = oEvent.getParameter("listItem");
+              var oSelectedValue = oSelectedItem.getCells()[0].getText();
+              var inputVoyageType = this.getView().byId("_IDInput2"); // Input field for Voyage Type
+              this.populateInputField(inputVoyageType, oSelectedValue);
+              oDialog.close();
+            }.bind(this),
+          }),
+          beginButton: new sap.m.Button({
+            text: "Cancel",
+            type: "Reject",
+            press: function () {
+              oDialog.close();
+            },
+          }),
+
+        });
+
+        let oValueHelpTable = oDialog.getContent()[0]; // Assuming the table is the first content element
+
+        oValueHelpTable.bindItems({
+          path: "/VOYTYP", // Replace with your entity set
+          template: new sap.m.ColumnListItem({
+            cells: [
+              new sap.m.Text({ text: "{VOYCD}" }),
+              new sap.m.Text({ text: "{VOYDES}" }),
+            ],
+          }),
+        });
+        // Bind the dialog to the view
+        this.getView().addDependent(oDialog);
+
+        // Open the dialog
+        oDialog.open();
+      },
+      showValueHelpDialog2: function () {
+        // Create a dialog
+        console.log("clicked Bidding type");
+        var oDialog = new sap.m.Dialog({
+          title: "Select: Bidding Types",
+          contentWidth: "60%",
+          contentHeight: "60%",
+          content: new sap.m.Table({
+            mode: sap.m.ListMode.SingleSelectMaster,
+
+            columns: [
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Bidding Type" }),
+              }),
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Description" }),
+              }),
+            ],
+            items: [
+                            new sap.m.ColumnListItem({
+                                cells: [
+                                    new sap.m.Text({ text: "TB" }),
+                                    new sap.m.Text({ text: "2 Bid System" }),
+                                ],
+                            }),
+                            new sap.m.ColumnListItem({
+                              cells: [
+                                  new sap.m.Text({ text: "SB" }),
+                                  new sap.m.Text({ text: "1 Bid System" }),
+                              ],
+                          }),
+
+                        // Add more ColumnListItems as needed
+                        ],
+
+            selectionChange: function (oEvent) {
+              var oSelectedItem = oEvent.getParameter("listItem");
+              var oSelectedValue = oSelectedItem.getCells()[0].getText();
+              var inputVoyageType = this.getView().byId("_IDInput3"); // Input field for Voyage Type
+              this.populateInputField(inputVoyageType, oSelectedValue);
+              oDialog.close();
+            }.bind(this),
+          }),
+          beginButton: new sap.m.Button({
+            text: "Cancel",
+            type: "Reject",
+            press: function () {
+              oDialog.close();
+            },
+          }),
+
+        });
+
+        let oValueHelpTable = oDialog.getContent()[0]; // Assuming the table is the first content element
+
+        // Bind the dialog to the view
+        this.getView().addDependent(oDialog);
+
+        // Open the dialog
+        oDialog.open();
+      },
+      showValueHelpDialog3: function () {
+        // Create a dialog
+        console.log("clicked Cargo type");
+        var oDialog = new sap.m.Dialog({
+          title: "Select: Vessel Types",
+          contentWidth: "60%",
+          contentHeight: "60%",
+          content: new sap.m.Table({
+            mode: sap.m.ListMode.SingleSelectMaster,
+
+            columns: [
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Vessel Type" }),
+              }),
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Description" }),
+              }),
+            ],
+
+            selectionChange: function (oEvent) {
+              var oSelectedItem = oEvent.getParameter("listItem");
+              var oSelectedValue = oSelectedItem.getCells()[0].getText();
+              var inputVoyageType = this.getView().byId("_IDInput4"); // Input field for Voyage Type
+              this.populateInputField(inputVoyageType, oSelectedValue);
+              oDialog.close();
+            }.bind(this),
+          }),
+          beginButton: new sap.m.Button({
+            text: "Cancel",
+            type: "Reject",
+            press: function () {
+              oDialog.close();
+            },
+          }),
+
+        });
+
+        let oValueHelpTable = oDialog.getContent()[0]; // Assuming the table is the first content element
+
+        oValueHelpTable.bindItems({
+          path: "/CARTYP", // Replace with your entity set
+          template: new sap.m.ColumnListItem({
+            cells: [
+              new sap.m.Text({ text: "{CARCD}" }),
+              new sap.m.Text({ text: "{CARDES}" }),
+            ],
+          }),
+        });
+        // Bind the dialog to the view
+        this.getView().addDependent(oDialog);
+
+        // Open the dialog
+        oDialog.open();
+      },
+      showValueHelpDialog4: function () {
+        // Create a dialog
+        console.log("clicked Currency type");
+        var oDialog = new sap.m.Dialog({
+          title: "Select: Vessel Types",
+          contentWidth: "60%",
+          contentHeight: "60%",
+          content: new sap.m.Table({
+            mode: sap.m.ListMode.SingleSelectMaster,
+
+            columns: [
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Currency Code" }),
+              }),
+              new sap.m.Column({
+                header: new sap.m.Text({ text: "Currency Description" }),
+              }),
+            ],
+
+            selectionChange: function (oEvent) {
+              var oSelectedItem = oEvent.getParameter("listItem");
+              var oSelectedValue = oSelectedItem.getCells()[0].getText();
+              var inputVoyageType = this.getView().byId("_IDInput5"); // Input field for Voyage Type
+              this.populateInputField(inputVoyageType, oSelectedValue);
+              oDialog.close();
+            }.bind(this),
+          }),
+          beginButton: new sap.m.Button({
+            text: "Cancel",
+            type: "Reject",
+            press: function () {
+              oDialog.close();
+            },
+          }),
+
+        });
+
+        let oValueHelpTable = oDialog.getContent()[0]; // Assuming the table is the first content element
+
+        oValueHelpTable.bindItems({
+          path: "/CURR", // Replace with your entity set
+          template: new sap.m.ColumnListItem({
+            cells: [
+              new sap.m.Text({ text: "{NAVOYCUR}" }),
+              new sap.m.Text({ text: "{NAVOYGCURDES}" }),
+            ],
+          }),
+        });
+        // Bind the dialog to the view
+        this.getView().addDependent(oDialog);
+
+        // Open the dialog
+        oDialog.open();
+      }
+
+
+    });
   });
-});
- 
-            
-        
-    
+
+
+
+
